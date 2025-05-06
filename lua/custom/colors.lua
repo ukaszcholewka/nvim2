fa69c58f8205cfd7eecb9396a0ae2d3e5c065029
Lukasz Cholewka <lukaszcholewka@loftyworks.com>
2025-05-06 09:33:52 +0200

    some colors

--- lua/custom/colors.lua
+++ lua/custom/colors.lua
@@ -8,6 +8,11 @@ function ColorMyPencils(color)
   vim.cmd.colorscheme(color)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.cmd("hi NormalNC guibg=NONE")
+  vim.cmd("hi FloatBorder guibg=NONE")
+  vim.cmd("hi SignColumn guibg=NONE")
+  -- vim.api.nvim_set_hl(0, "guibg", { color = "none" })
+  -- :hi Normal NC ctermbg=NONE guibg=NONE
 end
 
 ColorMyPencils("cyberdream")
